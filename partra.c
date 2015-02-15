709ecb9076583af19740b6fa88aa4e8d77d590f5
mike352 <mike352@users.noreply.github.com>
2015-02-16 01:42:28 +1100

    Fixed problem when selecting to give options through the command line

--- partra.c
+++ partra.c
@@ -26,7 +26,7 @@ r c A n m
 When H=0, m is not output
 
 Design principle:
-Only unsigned char or unsigned long long data types are used for consistency. The sole exception currently is the dcheck variable which checks whether a directory was successfully created, which is of type int because output from mkdir commands can be -1. 
+Only unsigned char or unsigned long long data types are used for integers for consistency. The sole exception currently is the dcheck variable which checks whether a directory was successfully created, which is of type int because output from mkdir commands can be -1. For strings, only char arrays of size 256 are used for consistency.
 
 To Add:
 1. Add api versions
@@ -34,8 +34,6 @@ To Add:
 3. Incorporate partra library into old equimodular curve finder program
 4. Write a portable bit architecture tester 
 5. Write a test suite
-
-
 */
 
 #include <stdio.h>
@@ -299,217 +297,68 @@ if (strcmp(option0,"y")==0)
 //Repeating Loop
 while ((strcmp(option0,"y")==0)&(repeat==1))
 {
-	if (strcmp(option0,"y")==0)
+	for (n=0;n<numoptions+1;n++)
 	{
-		for (n=0;n<numoptions+1;n++)
+		options[n] = (char*) malloc(256*sizeof(char));
+		if (options[n]==NULL)
 		{
-			options[n] = (char*) malloc(256*sizeof(char));
-			if (options[n]==NULL)
+			printf("ERROR: Could not allocate memory.\n");
+			for (m=0;m<n;m++)
 			{
-				printf("ERROR: Could not allocate memory.\n");
-				for (m=0;m<n;m++)
-				{
-					free(options[m]);
-				}
-				fclose(fid);
-				return 0;
+				free(options[m]);
 			}
+			fclose(fid);
+			return 0;
 		}
-		for (n=0;n<numoptions+1;n++)
+	}
+	for (n=0;n<numoptions+1;n++)
+	{
+		fcheck = fscanf(fid,"%s",options[n]);
+		if (fcheck!=1)
 		{
-			fcheck = fscanf(fid,"%s",options[n]);
-			if (fcheck!=1)
+			printf("ERROR: Problem reading option %d in the input file.",n+1);
+			for (n=0;n<numoptions+1;n++)
 			{
-				printf("ERROR: Problem reading option %d in the input file.",n+1);
-				for (n=0;n<numoptions+1;n++)
-				{
-					free(options[n]);
-				}
-				fclose(fid);
-				return 0;
+				free(options[n]);
 			}
+			fclose(fid);
+			return 0;
 		}
-		
-		//Choice of Model
-		strcpy(option1,options[0]);
-		if ((strcmp(option1,"i")!=0)&(strcmp(option1,"if")!=0)&(strcmp(option1,"p")!=0)&(strcmp(option1,"pf")!=0))
+	}
+	
+	//Choice of Model
+	strcpy(option1,options[0]);
+	if ((strcmp(option1,"i")!=0)&(strcmp(option1,"if")!=0)&(strcmp(option1,"p")!=0)&(strcmp(option1,"pf")!=0))
+	{
+		printf("\nERROR: Wrong model input value.");
+		flag=1;
+	}
+	
+	//Value of q (for Potts only)
+	if ((strcmp(option1,"p")==0)|(strcmp(option1,"pf")==0))
+	{
+		if (strspn(options[1],"1234567890")<strlen(options[1]))
 		{
-			printf("\nERROR: Wrong model input value.");
-			flag=1;
-		}
-		
-		//Value of q (for Potts only)
-		if ((strcmp(option1,"p")==0)|(strcmp(option1,"pf")==0))
-		{
-			if (strspn(options[1],"1234567890")<strlen(options[1]))
-			{
-				printf("\nERROR: Invalid q value."); //prevents negative values which would get reinterpreted, floats, and others
-				flag=1;
-			}
-			else
-			{
-				Q=strtoll(options[1],NULL,10);
-				if ((Q==LONG_MAX)|(Q==LONG_MIN))
-				{
-					printf("\nERROR: Given q value is too large. %s.",strerror(errno));
-					flag=1;
-				}
-				else if (Q==0)
-				{
-					printf("\nERROR: Invalid q value."); //problem converting string Qs to unsigned long long Q
-					flag=1;
-				}
-				else if (Q<3ULL)
-				{
-					printf("\nERROR: q should be greater than 2.");
-					flag=1;
-				}
-				else if (Q>=3ULL)
-				{
-					while((1ULL<<bin)<Q)
-					{
-						bin++; //calculate bit array bin size
-					}
-				}
-			}
-		}
-		
-		//Choice of row boundary condition
-		strcpy(option2,options[2]);
-		if ((strcmp(option2,"f")!=0)&(strcmp(option2,"c")!=0))
-		{
-			printf("\nERROR: Wrong row boundary condition input.");
-			flag=1;
-		}
-		
-		//Choice of full or reduced matrix
-		strcpy(option3,options[3]);
-		if ((strcmp(option3,"f")!=0)&(strcmp(option3,"r")!=0))
-		{
-			printf("\nERROR: Wrong input for choice of full or reduced matrix.");
-			flag=1;
-		}
-		
-		//Size of row
-		if (strspn(options[4],"1234567890")<strlen(options[4]))
-		{
-			printf("\nERROR: Invalid N value"); //prevents negative values which would get reinterpreted, floats, and others
+			printf("\nERROR: Invalid q value."); //prevents negative values which would get reinterpreted, floats, and others
 			flag=1;
 		}
 		else
 		{
-			N=strtoll(options[4],NULL,10);
-			if ((strcmp(option1,"i")==0)|(strcmp(option1,"if")==0))
-			{
-				if (N<1)
-				{
-					printf("\nERROR: Row size should be greater than 0.\n");
-					flag=1;
-				}
-				else if (N>row_max_size)
-				{
-					printf("\nERROR: Your machine can only do %d-bit calcuations.\n       Limit row size to %d.\n",row_max_size,row_max_size);
-					flag=1;
-				}
-			}
-			else if ((strcmp(option1,"p")==0)|(strcmp(option1,"pf")==0))
-			{
-				if (N<1)
-				{
-					printf("\nERROR: Row size should be greater than 0.\n");
-					flag=1;
-				}
-				else if (N>row_max_size/bin)
-				{
-					printf("\nERROR: Your machine can only do %d-bit calcuations.\n       Limit row size to %d.\n",row_max_size,row_max_size/(bin*N));
-					flag=1;
-				}
-			}
-		}
-		
-		//Choice of lattice
-		strcpy(option4,options[5]);
-		if ((strcmp(option4,"s")!=0)&(strcmp(option4,"t")!=0))
-		{
-			printf("\nERROR: Wrong lattice type input.");
-			flag=1;
-		}
-		
-		//Repeat program
-		strcpy(option5,options[6]);
-		if ((strcmp(option5,"y")!=0)&(strcmp(option5,"n")!=0))
-		{
-			printf("\nERROR: Wrong input of whether to continue reading input file.");
-			flag=1;
-		}
-		if (strcmp(option5,"n")==0) //last loop
-		{
-			repeat=0;
-			for (n=0;n<numoptions+1;n++)
-			{
-				free(options[n]);
-				
-			}
-			fclose(fid);
-		}
-		else 
-		{
-			if (fgets(tmp,2,fid)==NULL) //Skip a blank line
-			{
-				printf("\nERROR: Problem reading input file after option %d.",numoptions+1);
-				flag=1;
-			}			
-		}
-		
-		//Exit if any of the options were bad
-		if (flag==1)
-		{
-			for (n=0;n<numoptions+1;n++)
-			{
-				free(options[n]);
-				
-			}
-			fclose(fid);
-			return 0;
-		}
-		
-	}
-	else
-	{
-		//Model choice
-		printf("Ising or Ising in a field or Potts or Potts in a field  (i,if,p,pf): ");
-		scanf("%s",option1);
-		if ((strcmp(option1,"i")!=0)&(strcmp(option1,"if")!=0)&(strcmp(option1,"p")!=0)&(strcmp(option1,"pf")!=0))
-		{
-			printf("\nERROR: Wrong input.");
-			return 0;
-		}
-
-		//Input q value, checking carefully, since very large numbers are allowed. 
-		if ((strcmp(option1,"p")==0)|(strcmp(option1,"pf")==0))
-		{
-			printf("Potts q value  (3 to 2^%d-1): ",row_max_size);
-			scanf("%255s",Qs);
-			if (strspn(Qs,"1234567890")<strlen(Qs))
-			{
-				printf("\nERROR: Invalid q value.\n"); //prevents negative values which would get reinterpreted, floats, and others
-				return 0;
-			}
-			Q=strtoll(Qs,NULL,10);
+			Q=strtoll(options[1],NULL,10);
 			if ((Q==LONG_MAX)|(Q==LONG_MIN))
 			{
-				printf("\nERROR: Given q value is too large. %s.\n",strerror(errno));
-				return 0;
+				printf("\nERROR: Given q value is too large. %s.",strerror(errno));
+				flag=1;
 			}
 			else if (Q==0)
 			{
-				printf("\nERROR: Invalid q value.\n"); //problem converting string Qs to unsigned long long Q
-				return 0;
+				printf("\nERROR: Invalid q value."); //problem converting string Qs to unsigned long long Q
+				flag=1;
 			}
 			else if (Q<3ULL)
 			{
-				printf("\nERROR: q should be greater than 2.\n");
-				return 0;
+				printf("\nERROR: q should be greater than 2.");
+				flag=1;
 			}
 			else if (Q>=3ULL)
 			{
@@ -519,67 +368,106 @@ while ((strcmp(option0,"y")==0)&(repeat==1))
 				}
 			}
 		}
-
-		//Ask for row boundary condition
-		printf("Row boundary condition (f,c): ");
-		scanf("%s",option2);
-		if ((strcmp(option2,"f")!=0)&(strcmp(option2,"c")!=0))
-		{
-			printf("\nERROR: Wrong input.");
-			return 0;
-		}
-
-		//Ask for transfer matrix reduction
-		printf("Full or reduced transfer matrix (f,r): ");
-		scanf("%s",option3);
-		if ((strcmp(option3,"f")!=0)&(strcmp(option3,"r")!=0))
-		{
-			printf("\nERROR: Wrong input.");
-			return 0;
-		}
-
-		//Ask for row size
+	}
+	
+	//Choice of row boundary condition
+	strcpy(option2,options[2]);
+	if ((strcmp(option2,"f")!=0)&(strcmp(option2,"c")!=0))
+	{
+		printf("\nERROR: Wrong row boundary condition input.");
+		flag=1;
+	}
+	
+	//Choice of full or reduced matrix
+	strcpy(option3,options[3]);
+	if ((strcmp(option3,"f")!=0)&(strcmp(option3,"r")!=0))
+	{
+		printf("\nERROR: Wrong input for choice of full or reduced matrix.");
+		flag=1;
+	}
+	
+	//Size of row
+	if (strspn(options[4],"1234567890")<strlen(options[4]))
+	{
+		printf("\nERROR: Invalid N value"); //prevents negative values which would get reinterpreted, floats, and others
+		flag=1;
+	}
+	else
+	{
+		N=strtoll(options[4],NULL,10);
 		if ((strcmp(option1,"i")==0)|(strcmp(option1,"if")==0))
 		{
-			printf("Row size (1 to %hhu): ",row_max_size); 
-			scanf("%hhu",&N);
 			if (N<1)
 			{
 				printf("\nERROR: Row size should be greater than 0.\n");
-				return 0;
+				flag=1;
 			}
 			else if (N>row_max_size)
 			{
 				printf("\nERROR: Your machine can only do %d-bit calcuations.\n       Limit row size to %d.\n",row_max_size,row_max_size);
-				return 0;
+				flag=1;
 			}
 		}
 		else if ((strcmp(option1,"p")==0)|(strcmp(option1,"pf")==0))
 		{
-			printf("Row size (1 to %hhu): ",row_max_size/bin); 
-			scanf("%hhu",&N);
 			if (N<1)
 			{
 				printf("\nERROR: Row size should be greater than 0.\n");
-				return 0;
+				flag=1;
 			}
 			else if (N>row_max_size/bin)
 			{
 				printf("\nERROR: Your machine can only do %d-bit calcuations.\n       Limit row size to %d.\n",row_max_size,row_max_size/(bin*N));
-				return 0;
+				flag=1;
 			}
 		}
-
-		//Lattice
-		printf("Square or triangular lattice (s,t): ");
-		scanf("%s",option4);
-		if ((strcmp(option4,"s")!=0)&(strcmp(option4,"t")!=0))
-		{
-			printf("\nERROR: Wrong input.");
-			return 0;
-		}
 	}
-
+	
+	//Choice of lattice
+	strcpy(option4,options[5]);
+	if ((strcmp(option4,"s")!=0)&(strcmp(option4,"t")!=0))
+	{
+		printf("\nERROR: Wrong lattice type input.");
+		flag=1;
+	}
+	
+	//Repeat program
+	strcpy(option5,options[6]);
+	if ((strcmp(option5,"y")!=0)&(strcmp(option5,"n")!=0))
+	{
+		printf("\nERROR: Wrong input of whether to continue reading input file.");
+		flag=1;
+	}
+	if (strcmp(option5,"n")==0) //last loop
+	{
+		repeat=0;
+		for (n=0;n<numoptions+1;n++)
+		{
+			free(options[n]);
+			
+		}
+		fclose(fid);
+	}
+	else 
+	{
+		if (fgets(tmp,2,fid)==NULL) //Skip a blank line
+		{
+			printf("\nERROR: Problem reading input file after option %d.",numoptions+1);
+			flag=1;
+		}			
+	}
+	
+	//Exit if any of the options were bad
+	if (flag==1)
+	{
+		for (n=0;n<numoptions+1;n++)
+		{
+			free(options[n]);
+			
+		}
+		fclose(fid);
+		return 0;
+	}
 	//Choose a function
 	time(&tic);
 	if (strcmp(option4,"s")==0)
@@ -852,12 +740,389 @@ while ((strcmp(option0,"y")==0)&(repeat==1))
 			}
 		}
 	}
-if (flag==0)
-{
-	time(&toc);
-	totaltime = difftime(toc,tic);
-	printf("\n   The total time in seconds was %gs.",totaltime);
+	if (flag==0)
+	{
+		time(&toc);
+		totaltime = difftime(toc,tic);
+		printf("\n   The total time in seconds was %gs.",totaltime);
+	}
 }
+if (strcmp(option0,"n")==0) //Command line options
+{
+	//Model choice
+	printf("Ising or Ising in a field or Potts or Potts in a field  (i,if,p,pf): ");
+	scanf("%s",option1);
+	if ((strcmp(option1,"i")!=0)&(strcmp(option1,"if")!=0)&(strcmp(option1,"p")!=0)&(strcmp(option1,"pf")!=0))
+	{
+		printf("\nERROR: Wrong input.");
+		return 0;
+	}
+
+	//Input q value, checking carefully, since very large numbers are allowed. 
+	if ((strcmp(option1,"p")==0)|(strcmp(option1,"pf")==0))
+	{
+		printf("Potts q value  (3 to 2^%d-1): ",row_max_size);
+		scanf("%255s",Qs);
+		if (strspn(Qs,"1234567890")<strlen(Qs))
+		{
+			printf("\nERROR: Invalid q value.\n"); //prevents negative values which would get reinterpreted, floats, and others
+			return 0;
+		}
+		Q=strtoll(Qs,NULL,10);
+		if ((Q==LONG_MAX)|(Q==LONG_MIN))
+		{
+			printf("\nERROR: Given q value is too large. %s.\n",strerror(errno));
+			return 0;
+		}
+		else if (Q==0)
+		{
+			printf("\nERROR: Invalid q value.\n"); //problem converting string Qs to unsigned long long Q
+			return 0;
+		}
+		else if (Q<3ULL)
+		{
+			printf("\nERROR: q should be greater than 2.\n");
+			return 0;
+		}
+		else if (Q>=3ULL)
+		{
+			while((1ULL<<bin)<Q)
+			{
+				bin++; //calculate bit array bin size
+			}
+		}
+	}
+
+	//Ask for row boundary condition
+	printf("Row boundary condition (f,c): ");
+	scanf("%s",option2);
+	if ((strcmp(option2,"f")!=0)&(strcmp(option2,"c")!=0))
+	{
+		printf("\nERROR: Wrong input.");
+		return 0;
+	}
+
+	//Ask for transfer matrix reduction
+	printf("Full or reduced transfer matrix (f,r): ");
+	scanf("%s",option3);
+	if ((strcmp(option3,"f")!=0)&(strcmp(option3,"r")!=0))
+	{
+		printf("\nERROR: Wrong input.");
+		return 0;
+	}
+
+	//Ask for row size
+	if ((strcmp(option1,"i")==0)|(strcmp(option1,"if")==0))
+	{
+		printf("Row size (1 to %hhu): ",row_max_size); 
+		scanf("%hhu",&N);
+		if (N<1)
+		{
+			printf("\nERROR: Row size should be greater than 0.\n");
+			return 0;
+		}
+		else if (N>row_max_size)
+		{
+			printf("\nERROR: Your machine can only do %d-bit calcuations.\n       Limit row size to %d.\n",row_max_size,row_max_size);
+			return 0;
+		}
+	}
+	else if ((strcmp(option1,"p")==0)|(strcmp(option1,"pf")==0))
+	{
+		printf("Row size (1 to %hhu): ",row_max_size/bin); 
+		scanf("%hhu",&N);
+		if (N<1)
+		{
+			printf("\nERROR: Row size should be greater than 0.\n");
+			return 0;
+		}
+		else if (N>row_max_size/bin)
+		{
+			printf("\nERROR: Your machine can only do %d-bit calcuations.\n       Limit row size to %d.\n",row_max_size,row_max_size/(bin*N));
+			return 0;
+		}
+	}
+
+	//Lattice
+	printf("Square or triangular lattice (s,t): ");
+	scanf("%s",option4);
+	if ((strcmp(option4,"s")!=0)&(strcmp(option4,"t")!=0))
+	{
+		printf("\nERROR: Wrong input.");
+		return 0;
+	}
+	//Choose a function
+	time(&tic);
+	if (strcmp(option4,"s")==0)
+	{
+		if (strcmp(option1,"i")==0)
+		{
+			if ((strcmp(option2,"f")==0) & (strcmp(option3,"f")==0))
+			{
+				flag = i_sq_f(N,dirname);
+			}
+			else if ((strcmp(option2,"c")==0) & (strcmp(option3,"f")==0))
+			{
+				flag = i_sq_c(N,dirname);
+			}
+			else if ((strcmp(option2,"f")==0) & (strcmp(option3,"r")==0))
+			{
+				flag = i_sq_f_r(N,dirname);
+			}
+			else if ((strcmp(option2,"c")==0) & (strcmp(option3,"r")==0))
+			{
+				flag = i_sq_c_r(N,dirname);
+			}
+		}
+		else if (strcmp(option1,"if")==0)
+		{
+			if ((strcmp(option2,"f")==0) & (strcmp(option3,"f")==0))
+			{
+				flag = if_sq_f(N,dirname);
+			}
+			else if ((strcmp(option2,"c")==0) & (strcmp(option3,"f")==0))
+			{
+				flag = if_sq_c(N,dirname);
+			}
+			else if ((strcmp(option2,"f")==0) & (strcmp(option3,"r")==0))
+			{
+				flag = if_sq_f_r(N,dirname);
+			}
+			else if ((strcmp(option2,"c")==0) & (strcmp(option3,"r")==0))
+			{
+				flag = if_sq_c_r(N,dirname);
+			}
+		}
+		else if (strcmp(option1,"p")==0)
+		{
+			if ((strcmp(option2,"f")==0) & (strcmp(option3,"f")==0))
+			{
+				if (Q==(1<<bin))
+				{
+					flag = p2_sq_f(N,Q,dirname);
+				}
+				else
+				{
+					flag = p_sq_f(N,Q,dirname);
+				}
+			}
+			else if ((strcmp(option2,"c")==0) & (strcmp(option3,"f")==0))
+			{
+				if (Q==(1<<bin))
+				{
+					flag = p2_sq_c(N,Q,dirname);
+				}
+				else
+				{
+					flag = p_sq_c(N,Q,dirname);
+				}
+			}
+			else if ((strcmp(option2,"f")==0) & (strcmp(option3,"r")==0))
+			{
+				if (Q==(1<<bin))
+				{
+					flag = p2_sq_f_r(N,Q,dirname);
+				}
+				else
+				{
+					flag = p_sq_f_r(N,Q,dirname);
+				}
+			}
+			else if ((strcmp(option2,"c")==0) & (strcmp(option3,"r")==0))
+			{
+				if (Q==(1<<bin))
+				{
+					flag = p2_sq_c_r(N,Q,dirname);
+				}
+				else
+				{
+					flag = p_sq_c_r(N,Q,dirname);
+				}
+			}
+		}
+		else if (strcmp(option1,"pf")==0)
+		{
+			if ((strcmp(option2,"f")==0) & (strcmp(option3,"f")==0))
+			{
+				if (Q==(1<<bin))
+				{
+					flag = pf2_sq_f(N,Q,dirname);
+				}
+				else
+				{
+					flag = pf_sq_f(N,Q,dirname);
+				}
+			}
+			else if ((strcmp(option2,"c")==0) & (strcmp(option3,"f")==0))
+			{
+				if (Q==(1<<bin))
+				{
+					flag = pf2_sq_c(N,Q,dirname);
+				}
+				else
+				{
+					flag = pf_sq_c(N,Q,dirname);
+				}
+			}
+			else if ((strcmp(option2,"f")==0) & (strcmp(option3,"r")==0))
+			{
+				if (Q==(1<<bin))
+				{
+					flag = pf2_sq_f_r(N,Q,dirname);
+				}
+				else
+				{
+					flag = pf_sq_f_r(N,Q,dirname);
+				}
+			}
+			else if ((strcmp(option2,"c")==0) & (strcmp(option3,"r")==0))
+			{
+				if (Q==(1<<bin))
+				{
+					flag = pf2_sq_c_r(N,Q,dirname);
+				}
+				else
+				{
+					flag = pf_sq_c_r(N,Q,dirname);
+				}
+			}
+		}
+	}
+	else if (strcmp(option4,"t")==0)
+	{
+		if (strcmp(option1,"i")==0)
+		{
+			if ((strcmp(option2,"f")==0) & (strcmp(option3,"f")==0))
+			{
+				flag = i_tri_f(N,dirname);
+			}
+			else if ((strcmp(option2,"c")==0) & (strcmp(option3,"f")==0))
+			{
+				flag = i_tri_c(N,dirname);
+			}
+			else if ((strcmp(option2,"f")==0) & (strcmp(option3,"r")==0))
+			{
+				flag = i_tri_f_r(N,dirname);
+			}
+			else if ((strcmp(option2,"c")==0) & (strcmp(option3,"r")==0))
+			{
+				flag = i_tri_c_r(N,dirname);
+			}
+		}
+		else if (strcmp(option1,"if")==0)
+		{
+			if ((strcmp(option2,"f")==0) & (strcmp(option3,"f")==0))
+			{
+				flag = if_tri_f(N,dirname);
+			}
+			else if ((strcmp(option2,"c")==0) & (strcmp(option3,"f")==0))
+			{
+				flag = if_tri_c(N,dirname);
+			}
+			else if ((strcmp(option2,"f")==0) & (strcmp(option3,"r")==0))
+			{
+				flag = if_tri_f_r(N,dirname);
+			}
+			else if ((strcmp(option2,"c")==0) & (strcmp(option3,"r")==0))
+			{
+				flag = if_tri_c_r(N,dirname);
+			}
+		}
+		else if (strcmp(option1,"p")==0)
+		{
+			if ((strcmp(option2,"f")==0) & (strcmp(option3,"f")==0))
+			{
+				if (Q==(1<<bin))
+				{
+					flag = p2_tri_f(N,Q,dirname);
+				}
+				else
+				{
+					flag = p_tri_f(N,Q,dirname);
+				}
+			}
+			else if ((strcmp(option2,"c")==0) & (strcmp(option3,"f")==0))
+			{
+				if (Q==(1<<bin))
+				{
+					flag = p2_tri_c(N,Q,dirname);
+				}
+				else
+				{
+					flag = p_tri_c(N,Q,dirname);
+				}
+			}
+			else if ((strcmp(option2,"f")==0) & (strcmp(option3,"r")==0))
+			{
+				if (Q==(1<<bin))
+				{
+					flag = p2_tri_f_r(N,Q,dirname);
+				}
+				else
+				{
+					flag = p_tri_f_r(N,Q,dirname);
+				}
+			}
+			else if ((strcmp(option2,"c")==0) & (strcmp(option3,"r")==0))
+			{
+				if (Q==(1<<bin))
+				{
+					flag = p2_tri_c_r(N,Q,dirname);
+				}
+				else
+				{
+					flag = p_tri_c_r(N,Q,dirname);
+				}
+			}
+		}
+		else if (strcmp(option1,"pf")==0)
+		{
+			if ((strcmp(option2,"f")==0) & (strcmp(option3,"f")==0))
+			{
+				if (Q==(1<<bin))
+				{
+					flag = pf2_tri_f(N,Q,dirname);
+				}
+				else
+				{
+					flag = pf_tri_f(N,Q,dirname);
+				}
+			}
+			else if ((strcmp(option2,"c")==0) & (strcmp(option3,"f")==0))
+			{
+				if (Q==(1<<bin))
+				{
+					flag = pf2_tri_c(N,Q,dirname);
+				}
+				else
+				{
+					flag = pf_tri_c(N,Q,dirname);
+				}
+			}
+			else if ((strcmp(option2,"f")==0) & (strcmp(option3,"r")==0))
+			{
+				if (Q==(1<<bin))
+				{
+					flag = pf2_tri_f_r(N,Q,dirname);
+				}
+				else
+				{
+					flag = pf_tri_f_r(N,Q,dirname);
+				}
+			}
+			else if ((strcmp(option2,"c")==0) & (strcmp(option3,"r")==0))
+			{
+				if (Q==(1<<bin))
+				{
+					flag = pf2_tri_c_r(N,Q,dirname);
+				}
+				else
+				{
+					flag = pf_tri_c_r(N,Q,dirname);
+				}
+			}
+		}
+	}
 }
 
 return 0;
