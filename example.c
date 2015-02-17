b7de24bd7612a46a32e017c531eea624b659ac38
mike352 <mike352@users.noreply.github.com>
2015-02-18 03:16:09 +1100

    Added comments to example.c

--- example.c
+++ example.c
@@ -12,18 +12,17 @@ Matrix M;
 unsigned long long msize[2];
 char filename1[256],filename2[256];
 
-
+//Create and output first file
 flag = i_sq_f_f_file(N,dirname); 
 if (flag!=0)
 {
 	return 0;
 }
 
-
+//Second file - outputs should be equal
 flag = i_sq_f_f(&M,msize,filename1,N);
-
 sprintf(filename2,"%s/_%s",dirname,filename1);
-//printf("\nfilename = %s",filename2);
+printf("\nfilename2 = %s",filename2);
 fid = fopen(filename2,"w");
 for (n=0ULL;n<msize[0];n++)
 {
