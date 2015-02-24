36806c398a2616a3ee6aa90ace6ada2f8667ae6b
mike352 <mike352@users.noreply.github.com>
2015-02-24 23:57:31 +1100

    Still working on the substitutor function for gmp floats for accuracy. Not done yet.

--- src/genfuncs/genfuncs_mpf.c
+++ src/genfuncs/genfuncs_mpf.c
@@ -5,10 +5,10 @@
 There is a problem with this function. It needs to create a new matrix of a different type than the input matrix. 
 */
 
-unsigned char matrix_sub_mpf(unsigned char***** matrix, unsigned long long* msize, char* which, ...)
+unsigned char matrix_sub_mpf(double***** omatrix, unsigned long long* omsize, unsigned char**** imatrix, unsigned long long* imsize, char* which, ...)
 {
-unsigned long long n,m,p;
-unsigned char a,b;
+unsigned long long n,m,p,q;
+unsigned char ordering[2], remaining;
 mpf_t z,tmp;
 mpf_init2(z,21);
 mpf_init2(tmp,21);
@@ -19,57 +19,144 @@ mpf_set_d(z,va_arg(vl,double));
 
 if (strcmp(which,"u")==0)
 {
-	a=0;b=1;
+	if (imsize[1]==3)
+	{
+		ordering[0]=0;
+		ordering[1]=1;
+		remaining=1;
+	}
+	else if (imsize[1]==2)
+	{
+		ordering[0]=0;
+		remaining=0;
+	}
+	else
+	{
+		printf("ERROR: No free variables to use for substitution\n");
+		return 3;
+	}
+	omsize[1]=imsize[1]-1;
 }
 else if (strcmp(which,"x")==0)
 {
-	a=1;b=0;
+	if (imsize[1]==3)
+	{
+		ordering[0]=1;
+		ordering[1]=0;
+		remaining=1;
+	}
+	else if (imsize[1]==2)
+	{
+		ordering[0]=0;
+		remaining=0;
+	}
+	else
+	{
+		printf("ERROR: No free variables to use for substitution\n");
+		return 3;
+	}
+	omsize[1]=imsize[1]-1;
+}
+else if (strcmp(which,"ux")==0)
+{
+	ordering[0]=0;
+	ordering[1]=1;
+	remaining=0;
+	if (imsize[1]==2)
+	{
+		printf("ERROR: Not enough free variables to use for substitution\n");
+		return 3;
+	}
+	else if (imsize[1]<2)
+	{
+		printf("ERROR: No free variables to use for substitution\n");
+		return 3;
+	}
+	omsize[1]=imsize[1]-2;
+}
+else if (strcmp(which,"xu")==0)
+{
+	ordering[0]=1;
+	ordering[1]=0;
+	remaining=0;
+	if (imsize[1]==2)
+	{
+		printf("ERROR: Not enough free variables to use for substitution\n");
+		return 3;
+	}
+	else if (imsize[1]<2)
+	{
+		printf("ERROR: No free variables to use for substitution\n");
+		return 3;
+	}
+	omsize[1]=imsize[1]-2;
 }
 else
 {
-	printf("ERROR: Incorrect choice of variable substitution\n");
+	printf("ERROR: Incorrect choice of variable substitution. Only \"u\", \"x\", \"ux\", \"xu\" allowed.\n");
 	return 3;
 }
 
+omsize[0]=imsize[0];
+matrix_alloc_d(omatrix,omsize,imatrix[0][0][0][1]);  //allocate each row to previous row allocation
 
-unsigned long long rowsize=(*matrix)[0][0][0][0];
-double* row;
-row = calloc(2*rowsize,sizeof(double));
-
-for (n=0ULL;n<msize[0];n++)
+if (remaining==1)
 {
-	for (m=0ULL;m<msize[0];m++)
+	for (n=0ULL;n<omsize[0];n++)
 	{
-		if ((*matrix)[n][m][0][0]>rowsize)
+		for (m=0ULL;m<omsize[0];m++)
 		{
-			rowsize = (*matrix)[n][m][0][0];
-			row = realloc(row,2*rowsize*sizeof(double));
-			if (row==NULL)
+			if ((*omatrix)[n][m][0][1]<imatrix[n][m][0][1])
 			{
-				printf("ERROR: Unable to reallocate memory.\n");
-				free(row);
-				return 2;
+				(*omatrix)[n][m][0][1] = imatrix[n][m][0][0];
+				(*omatrix)[n][m][1] = (double*) realloc((*omatrix)[n][m][1],omsize[1]*imatrix[n][m][0][0]*sizeof(double));
+				if ((*omatrix)[n][m][1]==NULL)
+				{
+					printf("ERROR: Unable to reallocate memory.\n");
+					matrix_free_d((*omatrix),omsize);
+					return 2;
+				}
 			}
-		}
-		for (p=0;p<(*matrix)[n][m][0][0];p++)
-		{
-			row[2*p]=(*matrix)[n][m][1][msize[1]*p+a];
-			mpf_pow_ui(tmp,z,(*matrix)[n][m][1][msize[1]*p+b]);
-			mpf_mul_ui(tmp,tmp,(*matrix)[n][m][1][msize[1]*p+2]);
-			row[2*p+2] = mpf_get_d(tmp);
-		}
-		matrix[n][m][1] = realloc(matrix[n][m][1],2*(*matrix)[n][m][0][0]*sizeof(double));
-		if (matrix[n][m][1] == NULL)
-		{
-			printf("ERROR: Unable to reallocate memory.\n");
-			free(row);
-			return 2;
+			for (p=0;p<imatrix[n][m][0][0];p++)
+			{
+				(*omatrix)[n][m][1][omsize[1]*p]=imatrix[n][m][1][imsize[1]*p+ordering[0]];
+				mpf_pow_ui(tmp,z,imatrix[n][m][1][imsize[1]*p+ordering[1]]);
+				mpf_mul_ui(tmp,tmp,imatrix[n][m][1][imsize[1]*p+2]);
+				(*omatrix)[n][m][1][omsize[1]*p+1] = mpf_get_d(tmp);
+			}
 		}
 	}
 }
-msize[1]=2;
+else if (remaining==0)
+{
+	for (n=0ULL;n<omsize[0];n++)
+	{
+		for (m=0ULL;m<omsize[0];m++)
+		{
+			if ((*omatrix)[n][m][0][1]<imatrix[n][m][0][1])
+			{
+				(*omatrix)[n][m][0][1] = imatrix[n][m][0][0];
+				(*omatrix)[n][m][1] = (double*) realloc((*omatrix)[n][m][1],omsize[1]*imatrix[n][m][0][0]*sizeof(double));
+				if ((*omatrix)[n][m][1]==NULL)
+				{
+					printf("ERROR: Unable to reallocate memory.\n");
+					matrix_free_d((*omatrix),omsize);
+					return 2;
+				}
+			}
+			for (p=0;p<imatrix[n][m][0][0];p++)
+			{
+				for (q=0;q<imsize[1];q++) //continue working on this part 
+				{
+					mpf_pow_ui(tmp,z,imatrix[n][m][1][imsize[1]*p+ordering[q]]);
+					mpf_mul_ui(tmp,tmp,imatrix[n][m][1][imsize[1]*p+2]);
+				}
+				(*omatrix)[n][m][1][p] = mpf_get_d(tmp);
+			}
+		}
+	}
+}
 
 mpf_clear(tmp);
-free(row);
 return 0;
 }
