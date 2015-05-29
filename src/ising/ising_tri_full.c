98379c829773e73accaf401dda5dae12bfde2b23
Michael Assis <massis@entropy.complex.unimelb.edu.au>
2015-05-30 00:49:25 +1000

    added symmetric matrix functions to the ising square and triangular, full cases

--- src/ising/ising_tri_full.c
+++ src/ising/ising_tri_full.c
@@ -144,3 +144,154 @@ for(n=0; n<(1ULL<<N); n++)
 
 return 0;
 }
+
+
+
+/*****************************************************/
+/******************SYMMETRIC MATRICES*****************/
+/*****************************************************/
+
+/*******************************/
+unsigned char i_tri_f_f_s(unsigned char***** matrix, unsigned long long* msize, char* filename, const unsigned char N)
+{
+msize[1]=2ULL;
+sprintf(filename,"i_tri_f_f_%d",N);
+
+unsigned long long n;
+unsigned long long m;
+unsigned char uh,uh2;
+unsigned char flag;
+
+msize[0] = 1ULL<<N;
+flag = matrix_alloc(matrix,msize,1);
+if (flag!=0)
+{
+	return flag;
+}
+
+/*Free row boundary conditions*/
+for(n=0; n<(1ULL<<N); n++)
+{
+	uh = bit_sum((~1ULL&n)^(~1ULL&circ_single_lshift(n,N)));
+	for(m=0; m<(1ULL<<N); m++)
+	{
+		uh2 = bit_sum((~1ULL&m)^(~1ULL&circ_single_lshift(m,N)));
+		(*matrix)[n][m][0][0]=1;
+		(*matrix)[n][m][1][0]=2*bit_sum(n^m)+2*bit_sum((~1ULL&n)^(~1ULL&circ_single_lshift(m,N)))+uh+uh2;
+		(*matrix)[n][m][1][1]=1;
+	}
+}
+
+return 0;
+}
+
+
+/*******************************/
+unsigned char i_tri_c_f_s(unsigned char***** matrix, unsigned long long* msize, char* filename, const unsigned char N)
+{
+msize[1]=2ULL;
+sprintf(filename,"i_tri_c_f_%d",N);
+
+unsigned long long n;
+unsigned long long m;
+unsigned char uh,uh2;
+unsigned char flag;
+
+msize[0] = 1ULL<<N;
+flag = matrix_alloc(matrix,msize,1);
+if (flag!=0)
+{
+	return flag;
+}
+
+/*Cylindrical row boundary conditions*/
+for(n=0; n<(1ULL<<N); n++)
+{
+	uh = bit_sum(n^circ_single_lshift(n,N));
+	for(m=0; m<(1ULL<<N); m++)
+	{
+		uh2 = bit_sum(m^circ_single_lshift(m,N));
+		(*matrix)[n][m][0][0]=1;
+		(*matrix)[n][m][1][0]=2*bit_sum(n^m)+2*bit_sum(n^circ_single_lshift(m,N))+uh+uh2;
+		(*matrix)[n][m][1][1]=1;
+	}
+}
+
+return 0;
+}
+
+
+/*******************************/
+unsigned char if_tri_f_f_s(unsigned char***** matrix, unsigned long long* msize, char* filename, const unsigned char N)
+{
+msize[1]=3ULL;
+sprintf(filename,"if_tri_f_f_%d",N);
+
+unsigned long long n;
+unsigned long long m;
+unsigned char uh,uh2,xh,xh2;
+unsigned char flag;
+
+msize[0] = 1ULL<<N;
+flag = matrix_alloc(matrix,msize,1);
+if (flag!=0)
+{
+	return flag;
+}
+
+/*Free row boundary conditions*/
+for(n=0; n<(1ULL<<N); n++)
+{
+	uh = bit_sum((~1ULL&n)^(~1ULL&circ_single_lshift(n,N)));
+	xh = N-(bit_sum(n));
+	for(m=0; m<(1ULL<<N); m++)
+	{
+		uh2 = bit_sum((~1ULL&m)^(~1ULL&circ_single_lshift(m,N)));
+		xh2 = N-(bit_sum(m));
+		(*matrix)[n][m][0][0]=1;
+		(*matrix)[n][m][1][0]=2*bit_sum(n^m)+2*bit_sum((~1ULL&n)^(~1ULL&circ_single_lshift(m,N)))+uh+uh2;
+		(*matrix)[n][m][1][1]=xh+xh2;
+		(*matrix)[n][m][1][2]=1;
+	}
+}
+
+return 0;
+}
+
+
+/*******************************/
+unsigned char if_tri_c_f_s(unsigned char***** matrix, unsigned long long* msize, char* filename, const unsigned char N)
+{
+msize[1]=3ULL;
+sprintf(filename,"if_tri_c_f_%d",N);
+
+unsigned long long n;
+unsigned long long m;
+unsigned char uh,uh2,xh,xh2;
+unsigned char flag;
+
+msize[0] = 1ULL<<N;
+flag = matrix_alloc(matrix,msize,1);
+if (flag!=0)
+{
+	return flag;
+}
+
+/*Cylindrical row boundary conditions*/
+for(n=0; n<(1ULL<<N); n++)
+{
+	uh = bit_sum(n^circ_single_lshift(n,N));
+	xh = N-(bit_sum(n));
+	for(m=0; m<(1ULL<<N); m++)
+	{
+		uh2 = bit_sum(m^circ_single_lshift(m,N));
+		xh2 = N-(bit_sum(m));
+		(*matrix)[n][m][0][0]=1;
+		(*matrix)[n][m][1][0]=2*bit_sum(n^m)+2*bit_sum(n^circ_single_lshift(m,N))+uh+uh2;
+		(*matrix)[n][m][1][1]=xh+xh2;
+		(*matrix)[n][m][1][2]=1;
+	}
+}
+
+return 0;
+}
